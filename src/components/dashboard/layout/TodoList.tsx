81c8247b2e1744fe796387a8033c82a0eaa1510d
axellfumioo <axelazharputra@gmail.com>
2025-08-26 22:16:56 +0700

    fixfix

--- src/components/dashboard/layout/TodoList.tsx
+++ src/components/dashboard/layout/TodoList.tsx
@@ -46,7 +46,7 @@ export default function TodoList() {
     const fetchEvents = async () => {
       setIsLoading(true)
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/content?date=${selectedDateStr}`)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/content?date=${selectedDateStr}`)
         if (!res.ok) throw new Error('Failed to fetch events')
 
         const response = await res.json()
